746ab8836342e5827643411763281de573e6ab64
Anas <anas5678go@gmail.com>
2019-11-22 20:41:48 +0530

    random number

--- js/random.js
+++ js/random.js
@@ -6,3 +6,16 @@ let addRandomNumber = document.getElementById('addRandomNumber');
             random.innerHTML = a;
       
     });
+
+let createRandom = document.getElementById('createRandomNumber');
+    createRandom.addEventListener('click', func1);
+    function func1(){
+      let random = document.getElementById('random');  
+      let fromRandom = document.getElementById('fromrandom');
+      let toRandom = document.getElementById('torandom');
+      let b;
+      b = Math.floor(Number(fromRandom.value) + (toRandom.value - fromRandom.value + 1)*Math.random());
+        random.innerHTML = b;
+          }
+   
+  
